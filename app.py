5cd4a9fa5dde9134e8d30b3ba2df931397aedfa4
mankai-amine <medaminemankai@gmail.com>
2024-12-17 18:27:08 -0500

    Update db config

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 client = OpenAI(api_key=os.getenv("API_KEY"))
 
 # MySQL configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database
